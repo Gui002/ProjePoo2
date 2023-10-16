9e7786a6be7ebbca7ca6b59a5fe7aa72e0b74184
Gui <Gui@DESKTOP-SGE3POH>
2023-10-16 09:39:18 +0200

    Segundo commit

--- src/poo2_projecto/POO2_projecto.java
+++ src/poo2_projecto/POO2_projecto.java
@@ -15,7 +15,7 @@ public class POO2_projecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Guilherme Jacob Panguana");
     }
     
 }
